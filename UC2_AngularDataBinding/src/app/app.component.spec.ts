0fb260f7d49c3a9a332ab699d50f455a039b4289
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-03-05 17:20:46 +0530

    [Kapil Tiwari] UC3 of AngularDataBinding

--- UC2_AngularDataBinding/src/app/app.component.spec.ts
+++ UC2_AngularDataBinding/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'UC2_AngularDataBinding' title`, () => {
+  it(`should have the 'UC3_AngularDataBinding' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UC2_AngularDataBinding');
+    expect(app.title).toEqual('UC3_AngularDataBinding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC2_AngularDataBinding');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC3_AngularDataBinding');
   });
 });
