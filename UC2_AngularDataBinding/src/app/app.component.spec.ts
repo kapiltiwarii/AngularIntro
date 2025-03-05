e725390dba629ab5a0fb7c45c288d01a6c337865
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-03-05 17:19:46 +0530

    [Kapil Tiwari] UC2 of AngularDataBinding

--- UC2_AngularDataBinding/src/app/app.component.spec.ts
+++ UC2_AngularDataBinding/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'UC1_AngularDataBinding' title`, () => {
+  it(`should have the 'UC2_AngularDataBinding' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UC1_AngularDataBinding');
+    expect(app.title).toEqual('UC2_AngularDataBinding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC1_AngularDataBinding');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC2_AngularDataBinding');
   });
 });
--- UC2_AngularDataBinding/src/app/app.component.ts
+++ UC2_AngularDataBinding/src/app/app.component.ts
@@ -8,9 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'UC1_AngularDataBinding';
+  title = 'UC2_AngularDataBinding';
+  imgUrl = "https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75"
 
   ngOnInit(): void {
-       this.title="Hello From BridgeLabz";
-  }
+    this.title="Hello From BridgeLabz";
+}
 }
