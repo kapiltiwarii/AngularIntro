e2b11fdff46ac3a5dfaadef953028f971b3355c8
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-03-05 17:24:37 +0530

    [Kapil Tiwari] UC4 of AngularDataBinding

--- UC4_AngularDataBinding/src/app/app.component.spec.ts
+++ UC4_AngularDataBinding/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'UC3_AngularDataBinding' title`, () => {
+  it(`should have the 'UC4_AngularDataBinding' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UC3_AngularDataBinding');
+    expect(app.title).toEqual('UC4_AngularDataBinding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC3_AngularDataBinding');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UC4_AngularDataBinding');
   });
 });
--- UC4_AngularDataBinding/src/app/app.component.ts
+++ UC4_AngularDataBinding/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'UC3_AngularDataBinding';
+  title = 'UC4_AngularDataBinding';
   imgUrl = "https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75"
   url = "https://www.bridgelabz.com"
+
+  userName : string = '';
   ngOnInit(): void {
     this.title="Hello From BridgeLabz";
 }
@@ -19,4 +22,5 @@ onClick($event:any){
   console.log("Save button is clicked",$event);
   window.open(this.url,"_blank")
 }
+
 }
